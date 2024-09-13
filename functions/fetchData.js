3f9fbb2a84cf7fedb53c186d06f194fe9e34cb8d
nikonamayu3 <nikonama.yu3@gmail.com>
2024-09-13 14:57:35 +0900

    Add caching to fetchData function

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,24 +1,29 @@
 const fetch = require('node-fetch');
 
+// キャッシュ用の変数を定義
+let cachedData = null;
+let cacheTime = 0;
+const CACHE_DURATION = 5 * 60 * 1000; // キャッシュの有効期限 (5分)
+
 exports.handler = async function(event, context) {
   const API_KEY = process.env.API_KEY; // 環境変数からAPIキーを取得
   const CHANNEL_IDS = [
-'UCAflRAT6B_7nvxAK72ztN3A',
-'UCUuXWtrg4-1kGa3f7WEWzuA',
-'UCIOUnOw74BcQZzskbjK3f8w',
-'UCQjIXLk6lridUCQIBHaLMDw',
-'UCNwjdfFJVqQ9-qi1lwn4olw',
-'UCvCks1oVZfBuG1VXicOr5aw',
-'UCBxgYX7hJy6TZVvLXOEQrWw',
-'UCY85ViSyTU5Wy_bwsUVjkdA',
-'UC_wo1VD3xVl4Y_J15Wxm2ig',
-'UCBT07kDimH3-HJ9zq1rFQKg',
-'UCfvCFbU56UWX9ML0sOr9_iQ',
-'UCV1is8yPGubl7J7vG7K45Hg',
-'UCBvGU2rqWy2jiIeDBScdw1g',
-'UCRsgVmJAknvkTjoz2miNG5w',
-'UCV5jFxqHWDppyzznOBJbvVg',
-];
+    'UCAflRAT6B_7nvxAK72ztN3A',
+    'UCUuXWtrg4-1kGa3f7WEWzuA',
+    'UCIOUnOw74BcQZzskbjK3f8w',
+    'UCQjIXLk6lridUCQIBHaLMDw',
+    'UCNwjdfFJVqQ9-qi1lwn4olw',
+    'UCvCks1oVZfBuG1VXicOr5aw',
+    'UCBxgYX7hJy6TZVvLXOEQrWw',
+    'UCY85ViSyTU5Wy_bwsUVjkdA',
+    'UC_wo1VD3xVl4Y_J15Wxm2ig',
+    'UCBT07kDimH3-HJ9zq1rFQKg',
+    'UCfvCFbU56UWX9ML0sOr9_iQ',
+    'UCV1is8yPGubl7J7vG7K45Hg',
+    'UCBvGU2rqWy2jiIeDBScdw1g',
+    'UCRsgVmJAknvkTjoz2miNG5w',
+    'UCV5jFxqHWDppyzznOBJbvVg',
+  ];
 
   const fetchChannelVideos = async (channelId) => {
     try {
@@ -40,9 +45,22 @@ exports.handler = async function(event, context) {
     }
   };
 
+  // キャッシュが存在し、キャッシュが有効であればキャッシュからデータを返す
+  if (cachedData && (Date.now() - cacheTime) < CACHE_DURATION) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ schedule: cachedData }),
+    };
+  }
+
   try {
     const results = await Promise.all(CHANNEL_IDS.map(id => fetchChannelVideos(id)));
     const videos = results.flat();
+
+    // 新しいデータをキャッシュする
+    cachedData = videos;
+    cacheTime = Date.now();
+
     return {
       statusCode: 200,
       body: JSON.stringify({ schedule: videos }),
